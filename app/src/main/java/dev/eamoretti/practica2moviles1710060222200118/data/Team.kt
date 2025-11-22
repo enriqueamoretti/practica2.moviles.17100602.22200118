cc444b96382150771ca4f9be312cba12b2c4a3d3
Beatriz Bravo <beatrizbravo538@gmail.com>
2025-11-22 09:48:57 -0500

    feat: Agrega la pantalla de registro y el modelo de datos del equipo
    Agrega la pantalla `RegistroLigaScreen` y la pantalla `ListadoScreen` al grafo de navegación (`AppNavGraph`). La ruta inicial se establece en la pantalla de registro.
    
    Define el modelo de datos `Team` con los siguientes campos:
    - id
    - name
    - foundationYear
    - titleCount
    - imageUrl
    
    También se agregan los permisos de `INTERNET`, `READ_EXTERNAL_STORAGE` y `READ_MEDIA_IMAGES` en el `AndroidManifest.xml`.

---
+++ app/src/main/java/dev/eamoretti/practica2moviles1710060222200118/data/Team.kt
@@ -0,0 +1,8 @@
+package dev.eamoretti.practica2moviles1710060222200118.data
+data class Team(
+    val id: String = "",
+    val name: String = "",
+    val foundationYear: String = "",
+    val titleCount: String = "",
+    val imageUrl: String = ""
+)
