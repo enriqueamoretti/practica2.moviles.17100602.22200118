bc5f53ca9cb3a76ca26d636cf83c9e4e2061f2b8
Enrique Amoretti <enrique.eoal@gmail.com>
2025-11-22 08:26:46 -0500

    Initial commit

---
+++ app/src/main/java/dev/eamoretti/practica2moviles1710060222200118/MainActivity.kt
@@ -0,0 +1,47 @@
+package dev.eamoretti.practica2moviles1710060222200118
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import dev.eamoretti.practica2moviles1710060222200118.ui.theme.Practica2moviles1710060222200118Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Practica2moviles1710060222200118Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Practica2moviles1710060222200118Theme {
+        Greeting("Android")
+    }
+}
